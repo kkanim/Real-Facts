7ca49383c89cba167f1376734b6a6a93780341c8
kkanim <cliffordanim100@gmail.com>
2026-01-20 21:18:53 +0000

    Updated Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return(
     <header className="header">
       <nav>
-        <img className="logo" src="../react-1-logo-png-transparent.png" alt="React logo" />
+        <img className="logo" src="../images/react-1-logo-png-transparent.png" alt="React logo" />
         <span>ReactFacts</span>
       </nav>
     </header>
